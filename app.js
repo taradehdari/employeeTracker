732c079659bfac64b61f517b11eafccfc08fb375
Tara Dehdari <tara.dehdari@gmail.com>
2020-10-10 18:03:40 -0700

    tables using SQL works

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'me',
-    password : 'secret',
-    database : 'my_db'
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+class Database {
+  constructor(config) {
+    this.connection = mysql.createConnection(config);
+  }
+  query(sql, args) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(sql, args, (err, rows) => {
+        if(err)
+          return reject(err);
+        resolve(rows); 
+      });
+    });
+  }
+
+  close() {
+    return new Promise((resolve, reject) => {
+        this.connection.end(err => {
+            if (err)
+                return reject(err);
+            resolve();
+        });
+    });
+  }
+}
+
+const db = new Database({
+host: "localhost",
+port: 3306,
+user: "root",
+password: "bAVqss3GpRKQ",
+database: "employeeDB"
+});
+
+async function testFunc() {
+  await db.query('SELECT e.id, e.first_name AS First_Name, e.last_name AS Last_Name, title AS Title, salary AS Salary, name AS Department, CONCAT(m.first_name, " ", m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id', (err, res) => {
+      if (err) throw err;
+      console.table(res);
   });
+}
 
 
-  connection.connect();
- 
-  connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-  });
-   
-  connection.end();
+testFunc();
